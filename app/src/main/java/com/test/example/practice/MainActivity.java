d31d7451188b1991389d5a72044e623878886dd1
yc.huang <yc.huang@wavein.com.tw>
2016-09-20 12:01:23 +0800

    can run version

--- app/src/main/java/com/test/example/practice/MainActivity.java
+++ app/src/main/java/com/test/example/practice/MainActivity.java
@@ -1,13 +1,17 @@
 package com.test.example.practice;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
@@ -26,11 +30,16 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         mgr = (LocationManager) getSystemService(LOCATION_SERVICE);
     }
 
+    public void getPosition(View v) {
+        Intent it = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(it);
+    }
 
     @Override
     public void onLocationChanged(Location location) {
         String str = "Location Provider : " + location.getProvider();
-        str += String.format("\nLongitude: %.5f \n");
+        str += String.format("\nLongitude: %.5f \nLatitude: %.5f \n Altitude: %.2f", location.getLongitude(), location.getLatitude(), location.getAltitude());
+        txv.setText(str);
     }
 
     @Override
@@ -50,6 +59,27 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override
     protected void onResume() {
+        super.onPause();
+        String mProvider = mgr.getBestProvider(new Criteria(), true);
+
+        if (mProvider != null) {
+            txv.setText("Getting GPS Information... ");
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            mgr.requestLocationUpdates(mProvider, MIN_TIME, MIN_DIST, this);
+        }
+    }
+
+    @Override
+    protected void onPause() {
         super.onPause();
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
