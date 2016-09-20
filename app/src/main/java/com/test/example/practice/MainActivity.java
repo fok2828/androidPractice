0805784b63073054ab016fe9b87edc9e221e5c75
yc.huang <yc.huang@wavein.com.tw>
2016-09-20 11:28:27 +0800

    changing a little bit

--- app/src/main/java/com/test/example/practice/MainActivity.java
+++ app/src/main/java/com/test/example/practice/MainActivity.java
@@ -1,13 +1,67 @@
 package com.test.example.practice;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    static final int MIN_TIME = 5000;
+    static final float MIN_DIST = 5;
+    LocationManager mgr;
+    TextView txv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txv = (TextView) findViewById(R.id.txv);
+        mgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        String str = "Location Provider : " + location.getProvider();
+        str += String.format("\nLongitude: %.5f \n");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mgr.removeUpdates(this);
     }
 }
+
